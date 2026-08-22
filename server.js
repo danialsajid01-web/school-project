fc265cfc6a93e109685703d760f61bcd31a13520
danialsajid01-web <danialsajid01@gmail.com>
2026-08-22 09:46:41 -0700

    Update server.js

--- server.js
+++ server.js
@@ -346,6 +346,7 @@ app.delete('/delete-announcement/:id', (req, res) => {
 });
 
 // Start Server
-app.listen(PORT, '0.0.0.0', () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
