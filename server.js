3498c40a044309c756b334cb662ebcbe670f19aa
danialsajid01-web <danialsajid01@gmail.com>
2026-08-22 08:19:25 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json({ limit: '50mb' }));
